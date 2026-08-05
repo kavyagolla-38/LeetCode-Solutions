448099ce93aeb652afbe016210d7ea1dad7b4fd8
kavyagolla-38 <23BQ1A1238@vvit.net>
2026-08-05 22:58:14 +0530

    Time: 1 ms (98.94%), Space: 43 MB (56.28%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countAsterisks(String s) {
+        boolean inside=false;
+        int count=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='|'){
+                inside=!inside;
+            }
+            else if(ch=='*'&&!inside){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
